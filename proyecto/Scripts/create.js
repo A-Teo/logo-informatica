26eda87e31fa404236d428a20bc1a2ea84b275bc
David Zonneveld <davidzonneveld@gmail.com>
2014-04-18 17:12:19 -0400

    refactored the proyect in logical folders

---
+++ proyecto/Scripts/create.js
@@ -0,0 +1,52 @@
+﻿var crearElementos = {
+    esfera: function () {
+        var geometry = new THREE.SphereGeometry(10, 32, 32);
+        var material = new THREE.MeshNormalMaterial({ color: 0x70a0e0 });
+        var sphere = new THREE.Mesh(geometry, material);
+        return sphere;
+    },
+
+    textoRotatorio: function() {
+        var group = new THREE.Object3D();
+        group.add(crearElementos.texto());
+        return group;
+    },
+
+    texto: function () {
+        var text = "Informatica",
+            height = 1,
+            size = 6,
+
+            curveSegments = 4,
+
+            bevelThickness = 0.5,
+            bevelSize = 0.2,
+            bevelEnabled = true,
+
+            font = "optimer",
+            weight = "normal",
+            style = "normal";
+        var properties = {
+            size: size,
+            height: height,
+            curveSegments: curveSegments,
+
+            bevelThickness: bevelThickness,
+            bevelSize: bevelSize,
+            bevelEnabled: bevelEnabled,
+
+            font: font,
+            weight: weight,
+            style: style
+        };
+        var textGeo = new THREE.TextGeometry(text, properties);
+        var material = new THREE.MeshNormalMaterial({ color: 0x307090 });
+        textGeo.computeBoundingBox();
+        var centerOffset = -0.5 * (textGeo.boundingBox.max.x - textGeo.boundingBox.min.x);
+        var txt1 = new THREE.Mesh(textGeo, material);
+        txt1.position.x = centerOffset;
+        txt1.position.z = 12;
+        txt1.position.y = -3;
+        return txt1;
+    }
+}
---
+++ proyecto/Scripts/update.js
@@ -0,0 +1,25 @@
+﻿var actualizarElementos = {
+    esferaLogo: function () {
+        var esfera = elements["esfera"];
+        (
+            rotar = function () {
+                esfera.rotation.x += 0.1;
+                esfera.rotation.y += 0.1;
+                camera.position.z = 2;
+            }
+        )();
+    },
+    cubo1: function () {
+        var cube = elements["cube"];
+        (
+            rotar = function () {
+                //   cube.rotation.x += 0.1;
+                // cube.rotation.y += 0.1;
+                camera.position.z = 5;
+            }
+        )();
+    },
+    texto1: function () {
+        var texto = elements["texto1"];
+    }
+}
